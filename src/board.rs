79b2954975a63dd559aa3c8c49e4619a1974c899
George Lewis <ghl227@gmail.com>
2022-01-29 18:37:59 -0500

    Begin implementing board class

---
+++ src/board.rs
@@ -0,0 +1,38 @@
+
+
+
+// Representation of an Abalone board
+// Optimized for speed.
+
+//struct Row<const ROWSIZE: usize> {
+//    circles: [u8; ROWSIZE]
+//}
+
+type Circle = u8;
+
+// Number of spaces: 61
+//     X X X X X
+//    X X X X X X
+//   X X X X X X X
+//  X X X X X X X X
+// X X X X X X X X X
+//  X X X X X X X X
+//   X X X X X X X
+//    X X X X X X
+//     X X X X X
+pub struct Board {
+    circles: [Circle; 61]
+}
+
+
+// Implementation block, all `Point` associated functions & methods go in here
+impl Board {
+
+    pub fn new() -> Board {
+        Board{ circles: [0; 61]}
+    }
+
+    pub fn row(&self) -> &[Circle] {
+        &self.circles[0..10]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod board;
+
 fn main() {
+
+    let b = board::Board::new();
     println!("Hello, world!");
 }
